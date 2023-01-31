11fc05edab2e1237806fc7017474a4143500cee6
So Yeong <psykor48@gmail.com>
2023-01-31 10:58:18 +0900

    js 스크립트 수정

--- formValid.js
+++ formValid.js
@@ -1,5 +1,11 @@
+/**
+ * every  : 하나라도 false면 break
+ * @param  {object} formJson
+ * @param  {Array< {name : string , text : string }>} checkValidation
+ * @return {boolean} true => validation충족, false => 불충족
+ */
 function checkValidation(formJson, checkValidation) {
-    return checkValidation.every(function (item) {  //every : 모두 true / 하나라도 false이면 break
+    return checkValidation.every(function (item) {
         const vals = formJson[item.name];
         if (!vals) {
             // console.log(`${item.text} 을/를 입력해주세요.`)
@@ -20,33 +26,32 @@ function checkValidation(formJson, checkValidation) {
 
 
 /**
+ * some : 하나라도 true면 break
  * @param  {object} formJson
  * @param  {Array< {name : string , text : string }>} checkValidation
  * @return {boolean} true => validation충족, false => 불충족
  */
-//some으로 하는방법 : some - 하나라도 true면 break
-// function checkFormValidation2( formJson , checkValidation) {
-// 	let checkedNum = 0;
-// 	checkValidation.some(function(item ,idx){
-// 		const vals = formJson[item.name];
-// 		if (!vals) {
-// 			modal.showAlertModal('done',{ title: `${item.text} 을/를 입력해주세요.`, btn: '확인' },function(){
-// 				$(`[name="${item.name}"]`).focus();
-// 			});
-// 		}else {
-// 			//console.log(item.text +':' +  vals);
-// 			checkedNum++;
-// 		}
-// 		return (vals === null || vals === '' || vals === undefined);
-// 	});
-// 	return checkedNum === checkValidation.length;
-// }
+function checkFormValidation2(formJson, checkValidation) {
+    let checkedNum = 0;
+    checkValidation.some(function (item, idx) {
+        const vals = formJson[item.name];
+        if (!vals) {
+            modal.showAlertModal('done', { title: `${item.text} 을/를 입력해주세요.`, btn: '확인' }, function () {
+                $(`[name="${item.name}"]`).focus();
+            });
+        } else {
+            //console.log(item.text +':' +  vals);
+            checkedNum++;
+        }
+        return (vals === null || vals === '' || vals === undefined);
+    });
+    return checkedNum === checkValidation.length;
+}
 
 
 
-//for문 break버전
 /**
- * form의 필수요소값 체크 + 해당내용 입력swal기능.
+ * for문 break버전
  * key 배열일때는 값이 있을때만(length != 0 ) formData에 추가.
  * @param requiredFormObject 필수입력요소 { 'name' : '입력사항이름'} : object 
  * @param formData  :object
@@ -59,8 +64,8 @@ function checkIsFormFull(requiredFormObject, formData, text) {
         isFormFull = true;
         if (!formData[key]) {
             isFormFull = false;
-            swal2Alert(`${requiredFormObject[key]} 을/를 ${txt}해주세요.`, 3, function () {
-            });
+            // swal2Alert(`${requiredFormObject[key]} 을/를 ${txt}해주세요.`, 3, function () {
+            // });
             break;
         }
     }
